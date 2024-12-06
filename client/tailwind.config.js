0cc15b06bff4ae9539994792e79f1fa648733eca
HizkiaJustine <zeusxdhd@gmail.com>
2024-12-06 15:07:56 +0700

    benerin tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{html,js,vue,ts}',
+    './public/index.html',
+  ],
    darkMode: false, // or 'media' or 'class'
    theme: {
      extend: {},
