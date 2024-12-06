b6a50d4f467fa773278abc78457ce5a5aca7c992
6ill <kyrbill123@gmail.com>
2024-12-06 17:36:04 +0700

    Add jsonify to users controller

--- server/src/controllers/users_controller.py
+++ server/src/controllers/users_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session
+from flask import Blueprint, request, session, jsonify
 from src import db
 from src.models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,14 +15,21 @@ def register():
 
         if user:
             raise Exception("User with this username has already registered!")
+        
+        if not user:
+            raise Exception("No user")
+
+        if not password:
+            raise Exception("No password")
+        
         hashed_password = generate_password_hash(password)
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return {"message": "You have been register successfully!", "status": "success"}
+        return jsonify({"message": "You have been register successfully!", "status": "success"})
     except Exception as e:
         db.session.rollback()
-        return {"message": e, "status": "failed"}
+        return jsonify({"message": str(e), "status": "failed"})
 
 
 @users.route('/login', methods=['POST'])    
@@ -39,6 +46,6 @@ def login():
         session['user'] = {'id': user.id, 'username': user.username}
         session['is_authenticated'] = True
 
-        return {"message": "Login successfully", "status": "success"}
+        return jsonify({"message": "Login successfully", "status": "success"})
     except Exception as e: 
-        return {"message": e, "status": "failed"}
+        return jsonify({"message": str(e), "status": "failed"})
