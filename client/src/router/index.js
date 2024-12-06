7a2f2f221d47c5d818af7ff83fc6a5a48826b638
BuildTools <unconfigured@null.spigotmc.org>
2024-12-06 16:10:54 +0700

    Index.js routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
         component: () => import('../views/AboutView.vue'),
       },
       {
-        path: '/example', // Define the new route
+        path: '/example',
         name: 'Example',
         component: ExamplePage,
       }
