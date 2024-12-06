fdc64f719ebef128c486eb50bb629cf2c4c01308
6ill <kyrbill123@gmail.com>
2024-12-06 16:36:10 +0700

    Remove 'name' column.

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -5,6 +5,5 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True,)
-    name = db.Column(db.String(120), nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     
