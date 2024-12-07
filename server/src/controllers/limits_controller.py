8ee72d64feb9d58a8fd09af50b1fc3c62cca1e09
6ill <kyrbill123@gmail.com>
2024-12-07 15:42:04 +0700

    Get all limit data by user.

--- server/src/controllers/limits_controller.py
+++ server/src/controllers/limits_controller.py
@@ -5,7 +5,7 @@ from src import db
 limits = Blueprint("limits", __name__)
 
 
-@limits.route('/<int:user_id>', methods=['PUT'])
+@limits.route('/', methods=['PUT'])
 def update(user_id):
     try:
         data = request.json
@@ -27,3 +27,20 @@ def update(user_id):
     
     except Exception as e:
         return jsonify({"message": "Update budget failed. Please try again.", "status": "failed", "error": str(e)}), 500
+    
+@limits.route('/', methods=['GET'])
+def getBudgetLimit():
+    try:
+        user = session.get('user')
+        if not user: 
+            return jsonify({"message": "You have not logged in", "status": "failed"}), 401
+        
+        limit_data = Limit.query.filter_by(user_id=session['user']['id']).first()
+        if not limit_data:
+            return jsonify({"message": "Data not found", "status": "failed"}), 401
+        
+        
+        return jsonify({"limits": limit_data, "status": "success"}), 200
+    
+    except Exception as e:
+        return jsonify({"message": "Failed to fetch limit data.", "status": "failed", "error": str(e)}), 500
